0b34312d03c1889d5eb3272e27967eec5e0facc7
PeterWalchhofer <peter.walchhofer@liwest.at>
2021-04-24 11:25:32 +0200

    backup

--- README.md
+++ README.md
@@ -4,38 +4,21 @@
 
 
 # Merge script
-Apart from the jupyter notebook (Merge.ipynb), I implemented the merge-file-generator as a command-line tool.
-
-The wd merge script generates a matching file with common Wikidata ID.
+Apart from the jupyter notebook (Merge.ipynb), I implemented the merge-file-generator as a command-line tool. The wd merge script generates a matching file that links FtM Ids that have a common Wikidata ID and calls `ftm link`, which applies the merges and `ftm aggregate` to actually merge them (see [fragmentation](https://followthemoney.readthedocs.io/en/latest/fragments.html))
 ```
-python wd_merger.py match-wd -f <infile2> -s <infile1> -o <outpath>
+python wd_merger.py match-wd -f <infile2> -s <infile1> -o <outfile>
 ```
 
 You can copy an paste it and cd to root dir:
 ```
 python wd_merger.py match-wd -f data/output/everypolitician.json -s data/output/meineabgeordneten_wikidata.json -o data/output/matches.json
 ```
-<hr>
+This generates a `matches.json`, which can be used to stream to an Aleph instance or ingest into a Neo4j db.
 
-To apply the merge, we can use the `ftm link` command:
-```
-ftm link -i everypolitician.json -m matches.json -o linked_1.json
-```
+Aleph:
+```cat matches.json | alephclient --host https://aleph.occrp.org --api-key asdf123 write-entities -f collection_id ```
 
-```
-ftm link -i meineabgeordneten_wikidata.json -m matches.json -o linked_2.json
-```
-
-Now we have 2 files.
-
-```
-cat linked_1.json linked_2.json > matched.json
-```
-
-<br>
-
-Aggregate to actually merge them (see [fragmentation](https://followthemoney.readthedocs.io/en/latest/fragments.html))
-
-```
-cat matched.json | ftm aggregate -o matched_and_aggregated.json 
-```
+ Neo4j:
+ ```
+ cat matches.json | ftm export-cypher | cypher-shell -u user -p password
+ ```
--- wd_merger.py
+++ wd_merger.py
@@ -1,37 +1,55 @@
 import followthemoney as ftm
 import click
+from followthemoney.dedupe import Linker
 from followthemoney import model
+from followthemoney.cli import aggregate
+import followthemoney.cli.dedupe as dedupe
 from followthemoney.dedupe import Match
 from followthemoney.cli.cli import cli
 from followthemoney.cli.util import read_entities, write_object
 from followthemoney.exc import InvalidData
+from os.path import dirname
+
+MATCHES_FILE_NAME = "matches.json.temp" # matching file contains match objects
+MERGED_FILE_NAME = "matched_unaggr.json.temp" # merged file has unified IDs, but duplicates
+MATCHED_FILE_NAME = "matched.json" # final matched
 
 @click.group()
 def cli():
     pass
 
 @cli.command("match-wd", help="Match items with common Wikidata ID")
-@click.option("-f", "--first", type=click.File("r"), default="-")  # noqa
-@click.option("-s", "--second", type=click.File("r"), default="-")  # noqa
-@click.option("-o", "--outfile", type=click.File("w"), default="-")  # noqa
-def match_wd(first, second, outfile):
+@click.option("-f", "--first", type=click.File("r"), required=True)  # noqa
+@click.option("-s", "--second", type=click.File("r"), required=True)  # noqa
+@click.option("-o", "--outfile", type=click.File("w"), required=True)  # noqa
+@click.pass_context
+def match_wd(ctx, first, second, outfile):
     buffer = {}
-    #enricher = Enrichter()
+   
     try:
         matches = generate_matches(buffer, first)
         matches.extend(generate_matches(buffer, second))
         click.echo(f"Found {len(matches)} matches")
+
+        matchfile = open(dirname(outfile.name) + f"/{MATCHES_FILE_NAME}", "w")
         for match in matches:
-            write_object(outfile, match)
-           
+            write_object(matchfile, match)
+
+        merged_id_file_out = open(dirname(outfile.name) + f"/{MERGED_FILE_NAME}", "w")
+        ctx.invoke(dedupe.link, infile = open(first.name), outfile = merged_id_file_out, matches=open(matchfile.name))
+        ctx.invoke(dedupe.link, infile = open(second.name), outfile = merged_id_file_out, matches=open(matchfile.name))
+        
+        merged_id_file_in = open(merged_id_file_out.name)
+        ctx.invoke(aggregate.aggregate, infile = merged_id_file_in, outfile=outfile)
     except BrokenPipeError:
         raise click.Abort()
     
 
 
-def generate_matches(buffer, infile)
-"""Fill buffer and create match. This also finds duplicates within a file. """
+def generate_matches(buffer, infile):
+    """Fill buffer and create match. This also finds duplicates within a file. """
     matches = []
+    
     for entity in read_entities(infile):
         wd = entity.first("wikidataId", True)
         if wd and wd in buffer:
@@ -48,8 +66,8 @@ def make_match(entity, other):
     try:
         model.common_schema(entity.schema, other.schema)
     except InvalidData:
-        click.echo("EXC")
-        return # Do not match unmatchable types.
+        # Do not match unmatchable types.
+        return 
     
     match = Match(model, {})
     match.entity=  entity
