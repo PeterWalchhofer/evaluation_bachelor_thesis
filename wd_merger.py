931adb56bcbee8f8d3bab7fd18c8fd16d843abdc
PeterWalchhofer <peter.walchhofer@liwest.at>
2021-05-11 13:55:52 +0200

    generify merging cli

--- wd_merger.py
+++ wd_merger.py
@@ -1,5 +1,7 @@
 import followthemoney as ftm
+from followthemoney.exc import InvalidData
 import click
+from followthemoney.types import IdentifierType
 from followthemoney.dedupe import Linker
 from followthemoney import model
 from followthemoney.cli import aggregate
@@ -10,54 +12,82 @@ from followthemoney.cli.util import read_entities, write_object
 from followthemoney.exc import InvalidData
 from os.path import dirname
 
-MATCHES_FILE_NAME = "matches.json.temp" # matching file contains match objects
-MERGED_FILE_NAME = "matched_unaggr.json.temp" # merged file has unified IDs, but duplicates
-MATCHED_FILE_NAME = "matched.json" # final matched
+# matching file contains match objects
+MATCHES_FILE_NAME = "matches.json.temp"
+# merged file has unified IDs, but duplicates
+MERGED_FILE_NAME = "matched_unaggr.json.temp"
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command("match-wd", help="Match items with common Wikidata ID")
 @click.option("-f", "--first", type=click.File("r"), required=True)  # noqa
 @click.option("-s", "--second", type=click.File("r"), required=True)  # noqa
 @click.option("-o", "--outfile", type=click.File("w"), required=True)  # noqa
+@click.option("-p", "--property", required=False, help="Property to match on. Leave empty when matching on all identifiers.")  # noqa
 @click.pass_context
-def match_wd(ctx, first, second, outfile):
+def match_wd(ctx, first, second, outfile, property):
     buffer = {}
-   
+
     try:
-        matches = generate_matches(buffer, first)
-        matches.extend(generate_matches(buffer, second))
+        matches = generate_matches(buffer, first, property)
+        matches.extend(generate_matches(buffer, second, property))
         click.echo(f"Found {len(matches)} matches")
+
+        if len(matches) == 0:
+            return
 
         matchfile = open(dirname(outfile.name) + f"/{MATCHES_FILE_NAME}", "w")
         for match in matches:
             write_object(matchfile, match)
 
-        merged_id_file_out = open(dirname(outfile.name) + f"/{MERGED_FILE_NAME}", "w")
-        ctx.invoke(dedupe.link, infile = open(first.name), outfile = merged_id_file_out, matches=open(matchfile.name))
-        ctx.invoke(dedupe.link, infile = open(second.name), outfile = merged_id_file_out, matches=open(matchfile.name))
-        
+        merged_id_file_out = open(
+            dirname(outfile.name) + f"/{MERGED_FILE_NAME}", "w")
+        ctx.invoke(dedupe.link, infile=open(first.name),
+                   outfile=merged_id_file_out, matches=open(matchfile.name))
+        ctx.invoke(dedupe.link, infile=open(second.name),
+                   outfile=merged_id_file_out, matches=open(matchfile.name))
+
         merged_id_file_in = open(merged_id_file_out.name)
-        ctx.invoke(aggregate.aggregate, infile = merged_id_file_in, outfile=outfile)
+        ctx.invoke(aggregate.aggregate,
+                   infile=merged_id_file_in, outfile=outfile)
     except BrokenPipeError:
         raise click.Abort()
-    
 
 
-def generate_matches(buffer, infile):
-    """Fill buffer and create match. This also finds duplicates within a file. """
+
+ entity
+    return matches
+
+def generate_matches(buffer, infile,on):
     matches = []
     
     for entity in read_entities(infile):
-        wd = entity.first("wikidataId", True)
-        if wd and wd in buffer:
-            hit = buffer[wd]
-            match = make_match(hit, entity)
-            not match or matches.append(match)
-        if wd: 
-            buffer[wd] = entity
+        props = entity.properties
+        if on:
+            if entity.has(on, True):
+                props = [on] 
+            else:
+                continue
+            
+
+        for prop in props:
+            if entity.schema.properties[prop].type == IdentifierType:
+                id = entity.first(prop)
+                if not prop in buffer:
+                    buffer[prop] = {}
+
+                if id in buffer[prop]:
+                    hit = buffer[prop][id]
+                    match = make_match(hit, entity)
+                    not match or matches.append(match)
+                else:
+                    buffer[prop][id] = entity
+    
+    
     return matches
 
 
